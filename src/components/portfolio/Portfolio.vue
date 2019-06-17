cd840c4aaa46cab28361017c64ce311289ee3777
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-06-17 11:49:46 -0300

    Add files via upload

---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,22 @@
+<!--Mostrando a lista das compras na aba "Portfolio"-->
+<template>
+    <div>
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    import Stock from './Stock.vue';
+
+    export default {
+        computed: {
+            ...mapGetters({
+                stocks: 'stockPortfolio'
+            })
+        },
+        components: {
+            appStock: Stock
+        }
+    }
+</script>
